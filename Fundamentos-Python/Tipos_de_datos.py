311b9c930d390e2b327b2c3aff7fe7f1d66d03eb
Rebeca <espinarebeca8@gmail.com>
2024-10-14 16:25:26 -0600

    Modificado

--- Fundamentos-Python/Tipos_de_datos.py
+++ Fundamentos-Python/Tipos_de_datos.py
@@ -7,10 +7,14 @@
 # Variable - Una variable es un nombre que almacena un valor guardado en la memoria temporal.
 
 # Toda variable requiere un valor inicial
-semestre = 3        # es una variable que apunta a un objeto de tipo int con valor de 3
-print(semestre)     # imprime el valor de la variable
+semestre = 3        # Es una variable que apunta a un objeto de tipo int con valor de 3
+print(semestre)     # Imprime el valor de la variable
 semestre = 4        # la variable ya no apunta al objeto anterior, sino a uno nuevo, por lo que se pierde la referencia
 print(semestre)
 #Este programa imprimirá el valor de la variable semestre.
-# inicialmente en la primera línea imprimirá 3, pero en otra línea la variable semestre
-# se reasigna con otro valor causando que en la segunda linea al volver a imprimer la variable semestre arrojará en pantalla otra variable
+#Inicialmente en la primera línea imprimirá 3, pero en como dentro del código la variable semestre
+# se reasigna con otro valor, esta causa que al volver a imprimir la variable semestre arroje otro valor en la pantalla.
+
+#Resultado en pantalla:
+#3
+#4
