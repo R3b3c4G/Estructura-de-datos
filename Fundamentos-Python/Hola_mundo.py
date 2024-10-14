462e0853c25c70eb518b13423e0a803667c82c72
Rebeca <espinarebeca8@gmail.com>
2024-10-14 16:03:29 -0600

    Modificado

---
+++ Fundamentos-Python/Hola_mundo.py
@@ -0,0 +1,10 @@
+# Rebeca Gregorio Espina.
+# 8 de octubre de 2024.
+# Este es mi primer programa creado con Python.
+
+#Este pequeño programa imprimirá en pantalla la frase "Hola mundo".
+print("Hola mundo")
+#Notas:
+#La función print es para mostrar en pantalla.
+#A diferencia con el lenguaje C o C++, en Python no se agrega el f al print y tampoco llev
+# un punto y coma al final de cada línea de código.
--- Fundamentos-Python/Tipos_de_datos.py
+++ Fundamentos-Python/Tipos_de_datos.py
@@ -1,1 +1,16 @@
-print
+# Rebeca Gregorio Espina.
+# 8 de octubre de 2024.
+# En este archivo se ejemplifica el uso de variables en Python.
+
+# Notas:
+# En Python todo es un objeto.
+# Variable - Una variable es un nombre que almacena un valor guardado en la memoria temporal.
+
+# Toda variable requiere un valor inicial
+semestre = 3        # es una variable que apunta a un objeto de tipo int con valor de 3
+print(semestre)     # imprime el valor de la variable
+semestre = 4        # la variable ya no apunta al objeto anterior, sino a uno nuevo, por lo que se pierde la referencia
+print(semestre)
+#Este programa imprimirá el valor de la variable semestre.
+# inicialmente en la primera línea imprimirá 3, pero en otra línea la variable semestre
+# se reasigna con otro valor causando que en la segunda linea al volver a imprimer la variable semestre arrojará en pantalla otra variabl
