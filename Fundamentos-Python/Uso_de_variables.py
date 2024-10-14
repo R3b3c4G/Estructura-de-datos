d52dee686304b086ae264b7b37bb5ba4df7bd290
Rebeca <espinarebeca8@gmail.com>
2024-10-14 16:43:46 -0600

    Modificado

--- Fundamentos-Python/Uso_de_variables.py
+++ Fundamentos-Python/Uso_de_variables.py
@@ -12,11 +12,11 @@ print(semestre)     # Imprime el valor de la variable
 semestre = 4        # La variable ya no apunta al objeto anterior, sino a uno nuevo, por lo que se pierde la referencia
 print(semestre)
 #Este programa imprimirá el valor de la variable semestre.
-'''
+"""
 Inicialmente en la primera línea imprimirá 3, pero en como dentro del código la variable semestre
 se reasigna con otro valor, esta causa que al volver a imprimir la variable semestre arroje otro valor en la pantalla.
 
 #Resultado en pantalla:
 #3
 #4
-'''
+"""
