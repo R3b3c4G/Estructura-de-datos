6b047f433eefcc06125e3835e6d5a0113efc03d4
Rebeca <espinarebeca8@gmail.com>
2024-10-14 17:25:29 -0600

    Modificado

---
+++ Fundamentos-Python/Tipos_de_datos_ejercicio.py
@@ -0,0 +1,35 @@
+#Rebeca Gregorio Espina.
+#8 de octubre de 2024.
+"""
+Descripción: Este programa contiene algunos ejemplos del usos de los tipos
+básico de datos en Python tales como variables:
+- Int
+- Float
+- String
+- Boolean
+"""
+print("*** Gastos diarios ***")
+# En esta variable se almacena un valor de tipo entero
+mi_variable_dia = "Lunes"
+print("Día: ",mi_variable_dia)
+mi_variable_pasaje = 20
+print("Pasaje: ",mi_variable_pasaje)
+mi_variable_comida = 133.5
+print("Comida: ",mi_variable_comida)
+mi_variable_otros_gastos = 34.5
+print("Otros gastos: ",mi_variable_otros_gastos)
+mi_variable_pregunta_presupuesto = True
+print("¿Fue mayor a mi presupuesto?: ",mi_variable_pregunta_presupuesto)
+print()
+print("*** Gastos diarios ***")
+mi_variable_dia = "Martes"
+print("Día: ",mi_variable_dia)
+mi_variable_pasaje = 12
+print("Pasaje: ",mi_variable_pasaje)
+mi_variable_comida = 45.5
+print("Comida: ",mi_variable_comida)
+mi_variable_otros_gastos = 4.5
+print("Otros gastos: ",mi_variable_otros_gastos)
+
+mi_variable_pregunta_presupuesto = False
+print("¿Fue mayor a mi presupuesto?: ",mi_variable_pregunta_presupuesto)
