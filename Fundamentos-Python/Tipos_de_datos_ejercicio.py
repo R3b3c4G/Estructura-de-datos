0893212254968a6a0d567d50544749cc19dd7f08
Reb <espinarebeca8@gmail.com>
2024-10-14 20:11:56 -0600

    Modificado

--- Fundamentos-Python/Tipos_de_datos_ejercicio.py
+++ Fundamentos-Python/Tipos_de_datos_ejercicio.py
@@ -1,7 +1,7 @@
 #Rebeca Gregorio Espina.
 #8 de octubre de 2024.
 """
-Descripción: Este programa contiene algunos ejemplos del usos de los tipos
+Descripción: Este programa contiene algunos ejemplos del uso de los tipos
 básico de datos en Python tales como variables:
 - Int
 - Float
@@ -9,9 +9,12 @@ básico de datos en Python tales como variables:
 - Boolean
 """
 print("*** Gastos diarios ***")
+
 # En esta variable se almacena un valor de tipo entero
 mi_variable_dia = "Lunes"
 print("Día: ",mi_variable_dia)
+
+# En esta variable se almacena un valor de tipo entero
 mi_variable_pasaje = 20
 print("Pasaje: ",mi_variable_pasaje)
 mi_variable_comida = 133.5
