646bd8dd414561d19df8affb3588aadaab918d60
Rebeca <espinarebeca8@gmail.com>
2024-10-15 15:59:47 -0600

    Modificado

--- Operadores-en-Python/Entrada_consola.py
+++ Operadores-en-Python/Entrada_consola.py
@@ -12,7 +12,7 @@ resultado_cadena = numero1_cadena + numero2_cadena # Verificar qué es lo que re
 print()
 print(" ****  Recibir número sin un casting de varibles  ****")
 print(f"El resultado de {numero1_cadena} y {numero2_cadena} es: {resultado_cadena}")
-""" 
+
 # Comentar por qué se realiza el casting de variables.
 numero1_float = float(numero1_cadena)
 numero2_float = float(numero2_cadena)
@@ -20,4 +20,4 @@ resultado_float = numero1_float + numero2_float # Verificar qué es lo que reali
 print()
 print(" ****  Casting de varibles  ****")
 print(f"El resultado de {numero1_float} y {numero2_float} es: {resultado_float}")
-"""
+
