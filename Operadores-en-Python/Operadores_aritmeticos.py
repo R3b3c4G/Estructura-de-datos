8e2fa002b036b3d1c3ac9b271cd8f9317a0043e4
Rebeca <espinarebeca8@gmail.com>
2024-10-15 15:35:57 -0600

    Modificado

---
+++ Operadores-en-Python/Operadores_aritmeticos.py
@@ -0,0 +1,18 @@
+#Rebeca Gregorio Espina.
+#15 de octubre de 2024.
+
+
+#Suma   +
+#Resta  -
+#       *
+#       /
+#       %
+# ** Elevar a una potencia
+#Diferencia entre /  con flotantes
+# y // resultado entero
+
+numero1 = input("Introduce un número: ")
+numero2 = input("Introduce otro número: ")
+resultado = numero1 + numero2
+
+
