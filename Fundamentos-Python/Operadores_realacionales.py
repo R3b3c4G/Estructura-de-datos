14a0db6354c4ff197209ca9e3f53d28988032771
Reb <espinarebeca8@gmail.com>
2024-10-16 15:45:18 -0600

    Modificado

---
+++ Fundamentos-Python/Operadores_realacionales.py
@@ -0,0 +1,7 @@
+#Se utilizan para comparar dos valores
+numero1=87
+numero2=42.7
+print(f"¿El primer número es mayor?{numero1>numero2}")
+print(f"¿El primer número es menor?{numero1<numero2}")
+print(f"¿El primer número es igual?{numero1==numero2}")
+print(f"¿El primer número es diferente?{numero1!=numero2}")
