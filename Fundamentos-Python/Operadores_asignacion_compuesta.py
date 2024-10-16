aaf95aab2abdc1f1824626c5d2886c2d4058395f
Reb <espinarebeca8@gmail.com>
2024-10-16 15:26:21 -0600

    Modificado

---
+++ Fundamentos-Python/Operadores_asignacion_compuesta.py
@@ -0,0 +1,26 @@
+#16 de octubre de 2024.
+numero1=8
+numero2=3
+
+#numero1=numero1+3
+numero1+=3
+#numero2=numero2-5
+numero2-=5
+
+numero1*=2
+numero2/=4
+
+print("numero1:",numero1)
+print("numero2:",numero2)
+
+print("Segunda parte")
+numero1=8
+numero2=11
+
+numero1+=numero2
+numero1*=numero1
+numero1-=numero2
+numero1+=3
+numero1/=2
+print("numero1:",numero1)
+print("numero2:",numero2)
