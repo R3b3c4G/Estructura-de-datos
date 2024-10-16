ea498a34479e3b13de218fdcbeb9ecb804ca2605
Reb <espinarebeca8@gmail.com>
2024-10-16 15:53:12 -0600

    Modificado

--- Fundamentos-Python/Operadores_realacionales.py
+++ Fundamentos-Python/Operadores_realacionales.py
@@ -1,7 +1,8 @@
 #Se utilizan para comparar dos valores
 numero1=87
 numero2=42.7
-print(f"¿El primer número es mayor?{numero1>numero2}")
-print(f"¿El primer número es menor?{numero1<numero2}")
-print(f"¿El primer número es igual?{numero1==numero2}")
+print(f"¿El número {numero1:.2f} es mayor que {numero2:.2f}?{numero1>numero2}")
+print(f"¿{numero1:.2f} es mayor o igual que {numero2:.2f}?{numero1>=numero2}")
+print(f"¿{numero1:.2f} es menor que {numero2:.2f}?{numero1<numero2}")
+print(f"¿{numero1:.2f}es igual a {numero2:.2f}?{numero1==numero2}")
 print(f"¿El primer número es diferente?{numero1!=numero2}")
