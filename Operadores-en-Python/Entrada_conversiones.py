ee7addab3d7fccc013acc690cf0b864f53268249
Reb <espinarebeca8@gmail.com>
2024-10-21 22:33:18 -0600

    modificado

---
+++ Operadores-en-Python/Entrada_conversiones.py
@@ -0,0 +1,33 @@
+'''
+Nombre: Rebeca Gregorio Espina.
+Fecha: 21 de octubre de 2024.
+Descripción:
+Conversión de cadenas a int, float y boolean mediante la interacción con consola.
+'''
+
+# Comentar sobre las funciones anidadas.
+print("****   Datos de los alumnos    *****")
+nombre = input("Ingresa el nombre: ")
+# La función input() obtiene los datos ingresados por el usuario.
+semestre = int(input("Ingresa el no. de semestre: "))
+# Esta funcion anidada permite que el valor que el usuario ingrese se convierta directamente a un número entero.
+promedio = float(input("Ingresa el promedio: "))
+# Esta funcion anidada permite que el valor que el usuario ingrese se convierta directamente a un número decimal.
+es_mujer = input("¿Es mujer (Si/No)?: ")
+# No es posible convertir directamente una cadena a un valor booleano.
+# Por ello, utilizamos la misma variable, convertimos a  minúsculas y lo comparamos con la cadena "si".
+es_mujer = es_mujer.lower() == "si"
+# La función .lower convierte letras mayúsculas a minúsculas.
+
+# Se imprimen los datos del alumno.
+# Comentar  qué es lo que realiza {promedio:.2f} probando con números diferentes.
+print()
+print(f"{nombre} cursa el {semestre} semestre con un promedio de {promedio:.2f}. Además, es mujer: {es_mujer}.")
+"""
+# :.2f-->Es un formato de impresión en el que se especifica que se deben mostrar dos decimales.
+# Por lo tanto, para la variable promedio se mostrarán dos decimales.
+Ejemplo con diferentes valores:
+    -Ana cursa el 6 semestre con un promedio de 7.00. Además, es mujer: True.
+    -Raúl cursa el 3 semestre con un promedio de 8.00. Además, es mujer: False.
+    -Diego cursa el 9 semestre con un promedio de 9.00. Además, es mujer: False.
+"""
---
+++ Operadores-en-Python/Entrada_conversiones_ejercicio.py
@@ -0,0 +1,35 @@
+"""
+Nombre: Rebeca Gregorio Espina.
+Fecha: 21 de octubre de 2024.
+Descripción:
+Escribe un programa de nombre Entrada_conversiones_ejercicio.py que realice lo siguiente:
+    a) Pida los datos de los profesores utilizando nombres de variables adecuadas, la función input y el casting:
+        Nombre (cadena).
+        No. de cubículo (int).
+        Horas de clase que imparte a la semana (float con 3 decimales).
+        ¿Tiene más de 5 años en la unsij? (booleno).
+    b) Muestre los datos en consola de forma adecuada.
+
+    Nota: Asuma que el usuario siempre va a ingresar números cuando se requiera.
+"""
+
+# a) Pida los datos de los profesores utilizando nombres de variables adecuadas, la función input y el casting:
+
+nombre = input("Ingresa el nombre del profesor: ")
+# La función input() obtiene los datos ingresados por el usuario.
+numero_cubiculo = int(input("Ingresa el número de cubículo: "))
+# Esta funcion anidada permite que el valor que el usuario ingrese se convierta directamente a un número entero.
+horas_clase = float(input("Ingresa las horas de clase que imparte a la semana: "))
+# Esta funcion anidada permite que el valor que el usuario ingrese se convierta directamente a un número decimal.
+print()
+print(f"{nombre} se encuentra en el cubículo {numero_cubiculo} e imparte {horas_clase:.3f} horas de clase a la semana.")
+
+# b) Muestre los datos en consola de forma adecuada.
+"""
+Resultado en pantalla:
+    -Ingresa el nombre del profesor: Alberto Martínez Barbosa
+    -Ingresa el número de cubículo: 12
+    -Ingresa las horas de clase que imparte a la semana: 10
+
+    -Alberto Martínez Barbosa se encuentra en el cubículo 12 e imparte 10.000 horas de clase a la semana.
+"""
