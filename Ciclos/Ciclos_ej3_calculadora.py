f50f85779675a7849d14c1d52a739dead165f058
Reb <espinarebeca8@gmail.com>
2024-11-25 14:31:42 -0600

    Modificado

--- Ciclos/Ciclos_ej3_calculadora.py
+++ Ciclos/Ciclos_ej3_calculadora.py
@@ -22,183 +22,172 @@ Para ello:
     e) Repita el menú anterior.
 '''
 opcion = None   # None es una palabra reservada que se utiliza para definir un valor nulo o ningún valor.
-# def es una palabra reservada que se utiliza para definir una función.
-# Función para imprimir menú.
-def menu ():
-    print("     [1].- Suma.")
-    print("     [2].- Resta.")
-    print("     [3].- Multiplicación.")
-    print("     [4].- División.")
-    print("     [5].- División entera.")
-    print("     [6].-Potenciación.")
-    print("     [0].- Salir.")
-
-# Función para realizar los  cálculos.
-def calculadora (opcion, numero1, numero2): # c) Calcule el resultado de acuerdo con la opción.
-    if opcion == 1: # Suma.
-        resultado = numero1 + numero2
-    elif opcion == 2:   # Resta.
-        resultado= numero1 - numero2
-    elif opcion == 3:   # Multiplicación.
-        resultado = numero1 * numero2
-    elif opcion == 4:   # División.
-        resultado = numero1 / numero2
-    elif opcion == 5:   # División entera.
-        resultado = numero1 // numero2
-    elif opcion == 6:   # Potenciación.
-        resultado = numero1 ** numero2
-    return resultado    # Después de verificar qué caso aplica, se regresa a la función principal el resultado de la operación.
-
-while opcion != 0:  #  e) Repita el menú anterior.
-    print("***  Calculadora básica  ***")
-    menu()  # a) Muestre el menú.
-    print()
-    opcion = int(input("Elige una opción del menú anterior: "))
-    if opcion <= 6 and opcion >= 0:
-        if opcion != 0:  # b) Si la opción seleccionada fue entre 1 y 6, solicite dos números al usuario.
-            print()
-            numero1 = float(input("Ingresa un número: "))
-            numero2 = float(input("Ingresa otro número: "))
-            # Mando a llamar a la función calculadora con el argumento opcion, numero1 y numero2.
-            print(f"\nEl resultado es: {calculadora(opcion, numero1, numero2):.2f}")   # d) Muestre el resultado en pantalla.
-            print("----------------------------------\n")
+while opcion != 0 : # e) Repita el menú.
+    print("*** Calculadora básica***")  # a) Muestre el menú.
+    print("[1].- Suma.")
+    print("[2].- Resta.")
+    print("[3].- Multiplicación.")
+    print("[4].- División.")
+    print("[5].- División entera.")
+    print("[6].- Exponenciación.")
+    print("[0].- Salir.")
+    opcion = int(input("\nIngresa una opción del menú anterior: "))
+    if opcion >= 0 and opcion <= 6: # Cualquier otro caso -> Mostrar un mensaje de "opción no válida".
+        if opcion >= 1 and opcion <= 6: # b) Si la opción seleccionada fue entre 1 y 6, solicite dos números al usuario.
+            numero1 = float(input("\nIngrese el primer número: "))
+            numero2 = float(input("Ingresa el segundo número: "))
+            # c) Calcule el resultado de acuerdo a la opción.
+            if opcion == 1:
+                resultado = numero1 + numero2
+            elif opcion == 2:
+                resultado = numero1 - numero2
+            elif opcion == 3:
+                resultado = numero1 * numero2
+            elif opcion == 4:
+                resultado = numero1 / numero2
+            elif opcion == 5:
+                resultado = numero1 // numero2
+            elif opcion == 6:
+                resultado = numero1 ** numero2
+            print(f"\nEl resultado es: {resultado:.2f}")  # d) Muestre el resultado en pantalla.
+            print("----------------------------------------")
     else:
-        print("\nOpción no válida.") # Cualquier otro caso -> Mostrar un mensaje de "opción no válida".
-        print("----------------------------------\n")
-print("\nSaliendo...")
-print("----------------------------------\n")
-
+        print("\nOpción no válida.")
+        print("----------------------------------------")
+    print()
+print("Saliendo...")
+print("----------------------------------------")
 """
-# Resultado en consola:
-    ***  Calculadora básica  ***
-         [1].- Suma.
-         [2].- Resta.
-         [3].- Multiplicación.
-         [4].- División.
-         [5].- División entera.
-         [6].-Potenciación.
-         [0].- Salir.
+# Resultados en consola: 
+    *** Calculadora básica***
+    [1].- Suma.
+    [2].- Resta.
+    [3].- Multiplicación.
+    [4].- División.
+    [5].- División entera.
+    [6].- Exponenciación.
+    [0].- Salir.
     
-    Elige una opción del menú anterior: 1
+    Ingresa una opción del menú anterior: 1
     
-    Ingresa un número: 12.1
-    Ingresa otro número: 2.3
+    Ingrese el primer número: 12.1
+    Ingresa el segundo número: 2.3
     
     El resultado es: 14.40
-    ----------------------------------
+    ----------------------------------------
     
-    ***  Calculadora básica  ***
-         [1].- Suma.
-         [2].- Resta.
-         [3].- Multiplicación.
-         [4].- División.
-         [5].- División entera.
-         [6].-Potenciación.
-         [0].- Salir.
+    *** Calculadora básica***
+    [1].- Suma.
+    [2].- Resta.
+    [3].- Multiplicación.
+    [4].- División.
+    [5].- División entera.
+    [6].- Exponenciación.
+    [0].- Salir.
     
-    Elige una opción del menú anterior: 2
+    Ingresa una opción del menú anterior: 2
     
-    Ingresa un número: 1
-    Ingresa otro número: -1.3
+    Ingrese el primer número: 1
+    Ingresa el segundo número: -1.3
     
     El resultado es: 2.30
-    ----------------------------------
+    ----------------------------------------
     
-    ***  Calculadora básica  ***
-         [1].- Suma.
-         [2].- Resta.
-         [3].- Multiplicación.
-         [4].- División.
-         [5].- División entera.
-         [6].-Potenciación.
-         [0].- Salir.
+    *** Calculadora básica***
+    [1].- Suma.
+    [2].- Resta.
+    [3].- Multiplicación.
+    [4].- División.
+    [5].- División entera.
+    [6].- Exponenciación.
+    [0].- Salir.
     
-    Elige una opción del menú anterior: 3
+    Ingresa una opción del menú anterior: 3
     
-    Ingresa un número: -2.1
-    Ingresa otro número: 2
+    Ingrese el primer número: -2.1
+    Ingresa el segundo número: 2
     
     El resultado es: -4.20
-    ----------------------------------
+    ----------------------------------------
     
-    ***  Calculadora básica  ***
-         [1].- Suma.
-         [2].- Resta.
-         [3].- Multiplicación.
-         [4].- División.
-         [5].- División entera.
-         [6].-Potenciación.
-         [0].- Salir.
+    *** Calculadora básica***
+    [1].- Suma.
+    [2].- Resta.
+    [3].- Multiplicación.
+    [4].- División.
+    [5].- División entera.
+    [6].- Exponenciación.
+    [0].- Salir.
     
-    Elige una opción del menú anterior: 4
+    Ingresa una opción del menú anterior: 4
     
-    Ingresa un número: 10
-    Ingresa otro número: 3
+    Ingrese el primer número: 10
+    Ingresa el segundo número: 3
     
     El resultado es: 3.33
-    ----------------------------------
+    ----------------------------------------
     
-    ***  Calculadora básica  ***
-         [1].- Suma.
-         [2].- Resta.
-         [3].- Multiplicación.
-         [4].- División.
-         [5].- División entera.
-         [6].-Potenciación.
-         [0].- Salir.
+    *** Calculadora básica***
+    [1].- Suma.
+    [2].- Resta.
+    [3].- Multiplicación.
+    [4].- División.
+    [5].- División entera.
+    [6].- Exponenciación.
+    [0].- Salir.
     
-    Elige una opción del menú anterior: 5
+    Ingresa una opción del menú anterior: 5
     
-    Ingresa un número: 10
-    Ingresa otro número: 3
+    Ingrese el primer número: 10
+    Ingresa el segundo número: 3
     
     El resultado es: 3.00
-    ----------------------------------
+    ----------------------------------------
     
-    ***  Calculadora básica  ***
-         [1].- Suma.
-         [2].- Resta.
-         [3].- Multiplicación.
-         [4].- División.
-         [5].- División entera.
-         [6].-Potenciación.
-         [0].- Salir.
+    *** Calculadora básica***
+    [1].- Suma.
+    [2].- Resta.
+    [3].- Multiplicación.
+    [4].- División.
+    [5].- División entera.
+    [6].- Exponenciación.
+    [0].- Salir.
     
-    Elige una opción del menú anterior: 6
+    Ingresa una opción del menú anterior: 6
     
-    Ingresa un número: 2
-    Ingresa otro número: 3
+    Ingrese el primer número: 2
+    Ingresa el segundo número: 3
     
     El resultado es: 8.00
-    ----------------------------------
+    ----------------------------------------
     
-    ***  Calculadora básica  ***
-         [1].- Suma.
-         [2].- Resta.
-         [3].- Multiplicación.
-         [4].- División.
-         [5].- División entera.
-         [6].-Potenciación.
-         [0].- Salir.
+    *** Calculadora básica***
+    [1].- Suma.
+    [2].- Resta.
+    [3].- Multiplicación.
+    [4].- División.
+    [5].- División entera.
+    [6].- Exponenciación.
+    [0].- Salir.
     
-    Elige una opción del menú anterior: 1234
+    Ingresa una opción del menú anterior: 12
     
     Opción no válida.
-    ----------------------------------
+    ----------------------------------------
     
-    ***  Calculadora básica  ***
-         [1].- Suma.
-         [2].- Resta.
-         [3].- Multiplicación.
-         [4].- División.
-         [5].- División entera.
-         [6].-Potenciación.
-         [0].- Salir.
+    *** Calculadora básica***
+    [1].- Suma.
+    [2].- Resta.
+    [3].- Multiplicación.
+    [4].- División.
+    [5].- División entera.
+    [6].- Exponenciación.
+    [0].- Salir.
     
-    Elige una opción del menú anterior: 0
+    Ingresa una opción del menú anterior: 0
     
     Saliendo...
-    ----------------------------------
+    ----------------------------------------
 
 """
 
