d5889fa7fe81b786bb01d8b4421fa12e65bdf79c
Reb <espinarebeca8@gmail.com>
2024-12-08 23:41:43 -0600

    Modificado

---
+++ Ciclos/Ciclo_for.py
@@ -0,0 +1,37 @@
+# 6 de octubre de 2024
+# For variable in secuencia:
+# Ingresar una cadena
+contador = 1
+"""
+
+cadena_usuario = "Rebeca"
+for caracter in cadena_usuario:
+    contador= contador + 1
+    # R-e-b-e-c-a
+    print(f"{caracter}", end = "-")
+print(contador)
+
+"""
+"""
+alumnos = ["Rosalinda","Juan","Lourdes","Tania","Bryan","Rebeca","Jennifer","Héctor","Patricia","Alberto","Addi"]
+## Arreglo:
+## Listado: Se pueden almacenar difrentes tipos de datos.
+    #-Elemento individual.
+for alumno in alumnos:
+    contador = contador + 1
+    # R-e-b-e-c-a
+    print("Hola",alumno)
+print()
+print(contador)
+ """
+alumnos = ["Rosalinda","Juan","Lourdes","Tania","Bryan","Rebeca","Jennifer","Héctor","Patricia","Alberto","Addi"]
+## Arreglo:
+## Listado: Se pueden almacenar difrentes tipos de datos.
+    #-Elemento individual.
+
+for i in range(1,11):
+    # R-e-b-e-c-a
+    print(i, end = ", ")
+
+
+
